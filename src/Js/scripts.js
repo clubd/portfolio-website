1ab73506cdd6d00d3867b7019ec08b0fe47e43a9
clubd <danenrmar@gmail.com>
2025-04-11 22:23:58 -0400

    added functiont for toggleing navbar and styling for media query

--- src/Js/scripts.js
+++ src/Js/scripts.js
@@ -26,3 +26,12 @@ let header = document.querySelector('header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 
+// ===================toggle icon navbar =================
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
