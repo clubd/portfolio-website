6b89cb5fcfc99844f06a17cddcf2fd224da8a193
clubd <danenrmar@gmail.com>
2025-04-11 22:30:05 -0400

    added script to remove menu once section is click

--- src/Js/scripts.js
+++ src/Js/scripts.js
@@ -1,3 +1,13 @@
+// ==============================toggle icon navbar =========================================
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -20,18 +30,15 @@ window.addEventListener("scroll", () => {
             }
         }
     });
+
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // ====================remove toggle icon navbar when navbar link clicked ======================
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 });
-
-let header = document.querySelector('header');
-
-header.classList.toggle('sticky', window.scrollY > 100);
-
-// ===================toggle icon navbar =================
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
